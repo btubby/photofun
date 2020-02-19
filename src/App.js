730d82ea3ec2903d86e84ef6765812ae8c205f1b
Ben Tubby <btubby@gmail.com>
2020-02-19 20:36:10 +0000

    3 SwivelFace components

--- src/App.js
+++ src/App.js
@@ -1,6 +1,37 @@
 import React, {  useState } from 'react'
 import styled from 'styled-components';
-import Face from './componments/Face'
+import SwivelFace from './componments/Face'
+
+import kstraight from './kerry/straight.jpg'
+import kwest from './kerry/w.jpg'
+import keast from './kerry/e.jpg'
+import knorth from './kerry/n.jpg'
+import ksouth from './kerry/s.jpg'
+import knortheast from './kerry/ne.jpg'
+import knorthwest from './kerry/nw.jpg'
+import ksoutheast from './kerry/se.jpg'
+import ksouthwest from './kerry/sw.jpg'
+
+import wstraight from './will/straight.jpg'
+import wwest from './will/w.jpg'
+import weast from './will/e.jpg'
+import wnorth from './will/n.jpg'
+import wsouth from './will/s.jpg'
+import wnortheast from './will/ne.jpg'
+import wnorthwest from './will/nw.jpg'
+import wsoutheast from './will/se.jpg'
+import wsouthwest from './will/sw.jpg'
+
+import bstraight from './ben/straight.jpg'
+import bwest from './ben/w.jpg'
+import beast from './ben/e.jpg'
+import bnorth from './ben/n.jpg'
+import bsouth from './ben/s.jpg'
+import bnortheast from './ben/ne.jpg'
+import bnorthwest from './ben/nw.jpg'
+import bsoutheast from './ben/se.jpg'
+import bsouthwest from './ben/sw.jpg'
+
 
 const Container = styled.div`
   display:flex;
@@ -16,31 +47,21 @@ export default function App() {
     const [xCord, setxCord] = useState(0);
     const [yCord, setyCord] = useState(0);
 
-    const [ITop1, setITop1] = useState(0);
-    const [IBottom1, setIBottom1] = useState(0);
-    const [IRight1, setIRight1] = useState(0);
-    const [ILeft1, setILeft1] = useState(0);
+    const [N1, setN1] = useState(0);
+    const [S1, setS1] = useState(0);
+    const [E1, setE1] = useState(0);
+    const [W1, setW1] = useState(0);
  
-    const [ITop2, setITop2] = useState(0);
-    const [IBottom2, setIBottom2] = useState(0);
-    const [IRight2, setIRight2] = useState(0);
-    const [ILeft2, setILeft2] = useState(0);
+    const [N2, setN2] = useState(0);
+    const [S2, setS2] = useState(0);
+    const [E2, setE2] = useState(0);
+    const [W2, setW2] = useState(0);
  
-    const [ITop3, setITop3] = useState(0);
-    const [IBottom3, setIBottom3] = useState(0);
-    const [IRight3, setIRight3] = useState(0);
-    const [ILeft3, setILeft3] = useState(0);
-
-    const [ITop4, setITop4] = useState(0);
-    const [IBottom4, setIBottom4] = useState(0);
-    const [IRight4, setIRight4] = useState(0);
-    const [ILeft4, setILeft4] = useState(0);
-
-    const [ITop5, setITop5] = useState(0);
-    const [IBottom5, setIBottom5] = useState(0);
-    const [IRight5, setIRight5] = useState(0);
-    const [ILeft5, setILeft5] = useState(0);
-
+    const [N3, setN3] = useState(0);
+    const [S3, setS3] = useState(0);
+    const [E3, setE3] = useState(0);
+    const [W3, setW3] = useState(0);
+ 
     return (
       <Container
         onMouseMove={(e) => { 
@@ -49,67 +70,49 @@ export default function App() {
         }}
       >
         <div
-           ref={(el) => {
-            if (!el) return;
-            setITop1(el.getBoundingClientRect().top);
-            setIBottom1(el.getBoundingClientRect().bottom);
-            setIRight1(el.getBoundingClientRect().right);
-            setILeft1(el.getBoundingClientRect().left)
+           ref={(dd) => {
+            if (!dd) return;
+            setN1(dd.getBoundingClientRect().top);
+            setS1(dd.getBoundingClientRect().bottom);
+            setE1(dd.getBoundingClientRect().right);
+            setW1(dd.getBoundingClientRect().left)
           }}
         >
-          <Face type={'will'} xCord={xCord} yCord={yCord} ITop={ITop1} IBottom={IBottom1} IRight={IRight1} ILeft={ILeft1}/>
+          <SwivelFace  // will
+            ITop={N1} IBottom={S1} IRight={E1} ILeft={W1} xCord={xCord} yCord={yCord} 
+            straight={wstraight} w={wwest} e={weast} n={wnorth} s={wsouth} ne={wnortheast} nw={wnorthwest} se={wsoutheast} sw={wsouthwest}
+            />
         </div>
         
         <div
-           ref={(el2) => {
-            if (!el2) return;
-            setITop2(el2.getBoundingClientRect().top);
-            setIBottom2(el2.getBoundingClientRect().bottom);
-            setIRight2(el2.getBoundingClientRect().right);
-            setILeft2(el2.getBoundingClientRect().left)
+           ref={(dd) => {
+            if (!dd) return;
+            setN2(dd.getBoundingClientRect().top);
+            setS2(dd.getBoundingClientRect().bottom);
+            setE2(dd.getBoundingClientRect().right);
+            setW2(dd.getBoundingClientRect().left)
           }}
         >
-          <Face type={'will'} xCord={xCord} yCord={yCord}  ITop={ITop2} IBottom={IBottom2} IRight={IRight2} ILeft={ILeft2} />
+          <SwivelFace  // kerry
+            ITop={N2} IBottom={S2} IRight={E2} ILeft={W2} xCord={xCord} yCord={yCord} 
+            straight={kstraight} w={kwest} e={keast} n={knorth} s={ksouth} ne={knortheast} nw={knorthwest} se={ksoutheast} sw={ksouthwest}
+            />
         </div>
 
         <div
-           ref={(el3) => {
-            if (!el3) return;
-            setITop3(el3.getBoundingClientRect().top);
-            setIBottom3(el3.getBoundingClientRect().bottom);
-            setIRight3(el3.getBoundingClientRect().right);
-            setILeft3(el3.getBoundingClientRect().left)
+           ref={(dd) => {
+            if (!dd) return;
+            setN3(dd.getBoundingClientRect().top);
+            setS3(dd.getBoundingClientRect().bottom);
+            setE3(dd.getBoundingClientRect().right);
+            setW3(dd.getBoundingClientRect().left)
           }}
         >
-          <Face type={'will'} xCord={xCord} yCord={yCord} ITop={ITop3} IBottom={IBottom3} IRight={IRight3} ILeft={ILeft3} />
+          <SwivelFace  // ben
+            ITop={N3} IBottom={S3} IRight={E3} ILeft={W3} xCord={xCord} yCord={yCord} 
+            straight={bstraight} w={bwest} e={beast} n={bnorth} s={bsouth} ne={bnortheast} nw={bnorthwest} se={bsoutheast} sw={bsouthwest}
+          />
         </div>
-
-
-        <div
-           ref={(el4) => {
-            if (!el4) return;
-            setITop4(el4.getBoundingClientRect().top);
-            setIBottom4(el4.getBoundingClientRect().bottom);
-            setIRight4(el4.getBoundingClientRect().right);
-            setILeft4(el4.getBoundingClientRect().left)
-          }}
-        >
-          <Face type={'will'} xCord={xCord} yCord={yCord} ITop={ITop4} IBottom={IBottom4} IRight={IRight4} ILeft={ILeft4} />
-        </div>
-
-
-        <div
-           ref={(el5) => {
-            if (!el5) return;
-            setITop5(el5.getBoundingClientRect().top);
-            setIBottom5(el5.getBoundingClientRect().bottom);
-            setIRight5(el5.getBoundingClientRect().right);
-            setILeft5(el5.getBoundingClientRect().left)
-          }}
-        >
-          <Face type={'will'} xCord={xCord} yCord={yCord} ITop={ITop5} IBottom={IBottom5} IRight={IRight5} ILeft={ILeft5} />
-        </div>
-
 
       </Container>
     );
--- src/componments/Face.js
+++ src/componments/Face.js
@@ -1,22 +1,4 @@
 import React, {  useState,useEffect } from 'react'
-import kstraight from '../kerry/straight.jpg'
-import kwest from '../kerry/w.jpg'
-import keast from '../kerry/e.jpg'
-import knorth from '../kerry/n.jpg'
-import ksouth from '../kerry/s.jpg'
-import knortheast from '../kerry/ne.jpg'
-import knorthwest from '../kerry/nw.jpg'
-import ksoutheast from '../kerry/se.jpg'
-import ksouthwest from '../kerry/sw.jpg'
-import wstraight from '../will/straight.jpg'
-import wwest from '../will/w.jpg'
-import weast from '../will/e.jpg'
-import wnorth from '../will/n.jpg'
-import wsouth from '../will/s.jpg'
-import wnortheast from '../will/ne.jpg'
-import wnorthwest from '../will/nw.jpg'
-import wsoutheast from '../will/se.jpg'
-import wsouthwest from '../will/sw.jpg'
 
 import styled from 'styled-components';
 
@@ -26,60 +8,40 @@ const Container = styled.div`
     width: 100%;
 `;
 const IMG = styled.img`
-    width:100%;
+    width:80%;
     height:auto;
 `;
 
-export default function Face(props) {
-    const {width='100%', xCord, yCord, type, ITop, IBottom, IRight, ILeft} = props;
-
-    let straight = wstraight;
-    let west = wwest;
-    let east = weast;
-    let north = wnorth;
-    let south = wsouth;
-    let northeast = wnortheast;
-    let northwest = wnorthwest;
-    let southeast = wsoutheast;
-    let southwest = wsouthwest;
-
-    if (type === 'kerry') {
-         straight = kstraight;
-         west = kwest;
-         east = keast;
-         north = knorth;
-         south = ksouth;
-         northeast = knortheast;
-         northwest = knorthwest;
-         southeast = ksoutheast;
-         southwest = ksouthwest;
-    }
+export default function SwivelFace(props) {
+    const {xCord, yCord, type, ITop, IBottom, IRight, ILeft, 
+    straight, w, e, n, s, nw, ne, sw,se} = props;
     
     const [image, setImage] = useState(straight);
+
     useEffect(() => {
         let map = { n:0, e:0, s:0, w:0}
         console.log(`${type} top: ${ITop} bottom: ${IBottom} right: ${IRight} left: ${ILeft}` ); 
 
-        if (yCord <= ITop) {map.n=1; }
+        if (yCord <= ITop) { map.n=1 }
         if (xCord >= IRight ){ map.e=1 }
-        if (xCord <= ILeft ) { map.w=1; }
-        if (yCord >= IBottom ){ map.s=1; }
+        if (xCord <= ILeft ) { map.w=1 }
+        if (yCord >= IBottom ){ map.s=1 }
         
-        console.log(`${type}`, map) 
+        // console.log(`${type}`, map) 
         setImage(straight)
-        if (map.n) { setImage(north) }
-        if (map.e) { setImage(east) }
-        if (map.n && map.e){ setImage(northeast) }
-        if (map.s) { setImage(south) }
-        if (map.s && map.e){ setImage(southeast) }
-        if (map.w) { setImage(west) }
-        if (map.s && map.w){ setImage(southwest) }
-        if (map.w && map.n){ setImage(northwest) }
+        if (map.n) { setImage(n) }
+        if (map.e) { setImage(e)}
+        if (map.n && map.e){ setImage(ne) }
+        if (map.s) { setImage(s) }
+        if (map.s && map.e){ setImage(se) }
+        if (map.w) { setImage(w) }
+        if (map.s && map.w){ setImage(sw) }
+        if (map.w && map.n){ setImage(nw) }
       });
       return (
           <Container
           >
-            <IMG width={width} src={image} alt="{image}" />
+            <IMG src={image} alt="{image}" />
           </Container>
       )
   }
